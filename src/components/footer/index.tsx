fbbff1c6dcd1360a0041e1ba78f5765957d8031e
Carlos Cerqueira <ce.cesc01@gmail.com>
2022-04-19 12:36:13 -0300

    feat: add footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import styles from "./styles.module.css";
 
 export default function Footer() {
-    return(
-        <footer>
-            this is a footer
-        </footer>
-    )
+  return (
+    <footer>
+      <p className={styles.DevNameInfo}>
+        Feito com <FontAwesomeIcon icon={faHeart} /> por Carlos Cerqueira
+      </p>
+      <div className={styles.ProjectInfo}>
+        Saiba mais sobre o projeto:
+        <ul className={styles.ProjectInfoList}>
+          <li>
+            <a href="https://github.com/Carloscerq/eureka-challenge-frontend">Frontend</a>
+          </li>
+          <li>
+            <a href="https://github.com/Carloscerq/eureka-challenge-backend">
+              Backend
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
