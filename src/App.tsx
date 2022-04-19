b1627caaeb9a109178bd68892af521b6887c6eb4
Carlos Cerqueira <ce.cesc01@gmail.com>
2022-04-19 19:31:47 -0300

    fix: fix footer position

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Main from "./components/main";
+import styles from "./styles.module.css";
 
 function App() {
   return (
-    <>
+    <div className={styles.root}>
       <Header />
       <Main />
       <Footer />
-    </>
+    </div>
   );
 }
 
