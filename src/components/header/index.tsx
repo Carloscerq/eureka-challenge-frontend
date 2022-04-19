4fb1fa4e5858e27c743a6cecc6734b31317deccd
Carlos Cerqueira <ce.cesc01@gmail.com>
2022-04-19 11:59:26 -0300

    feat: add header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import style from "./styles.module.css";
 
 export default function Header() {
   return (
     <header>
-      <h1>Hello, world!</h1>
-      <p>This is a boilerplate for a React app.</p>
+      <h3 className={style.Title}>CepAPI</h3>
+      <div className={style.SocialMedia}>
+        <a href="https://github.com/Carloscerq">
+          <FontAwesomeIcon
+            className={style.SocialMediaIcon}
+            icon={faGithub}
+            size="3x"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/carlos-cerqueira-5153991aa/">
+          <FontAwesomeIcon
+            className={style.SocialMediaIcon}
+            icon={faLinkedin}
+            size="3x"
+          />
+        </a>
+      </div>
     </header>
   );
 }
